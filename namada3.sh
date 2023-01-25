239237802e6deeeff4ddfea16d6cb489148929bf
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2023-01-25 22:07:17 +0200

    Update namada3.sh

--- namada3.sh
+++ namada3.sh
@@ -38,7 +38,7 @@ echo "============================================================"
 echo "Install start"
 echo "============================================================"
 
-NAMADA_TAG=v0.13.2
+NAMADA_TAG=v0.13.3
 TM_HASH=v0.1.4-abciplus
 CHAIN_ID="public-testnet-2.1.4014f207f6d"
 
